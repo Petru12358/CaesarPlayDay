94dfca63b6f9b5c35cc8088cf3b107ed07bb7e3e
Vlad Drahnea <vlad.drahnea@gmail.com>
2015-08-14 15:29:31 +0300

    Added arguments description

---
+++ caesar.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+from subprocess import check_output
+from sys import argv
+
+# -e --encrypt encrypt a file
+# -d --decrypt decrupt a file
+# -i --input   input filename
+# -o --output  output filename
+# -s --stdo    not use file for output
+
